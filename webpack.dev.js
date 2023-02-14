5ac27fd5602260bde44c23a4bd955480a37a794c
Михаил Солонский <msolonsky@gmail.com>
2023-02-14 20:46:54 +0100

    init project

---
+++ webpack.dev.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  // Set the mode to development or production
+  mode: 'development',
+  // Control how source maps are generatedwe
+  devtool: 'inline-source-map',
+
+  // Spin up a server for quick development
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, './dist'),
+    },
+    open: true,
+    compress: true,
+    port: 8080,
+    hot: true,
+  },
+
+  plugins: [
+    // Only update what has changed on hot reload
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+});
---
+++ webpack.prod.js
@@ -0,0 +1,14 @@
+const { merge } = require('webpack-merge');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      '...',
+    ],
+  },
+});
